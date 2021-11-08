0f7e8ff7cee9083583b0c939678bc564d4d51774
umuttaskiran <umut.taskiran7@gmail.com>
2021-11-08 23:26:36 +0300

    add SignUpLeftSideTexts component

---
+++ src/components/SignUpLeftSideTexts.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  name: "SignUpLeftSideTexts",
+};
+</script>
+
+<template>
+  <div>
+    <article>
+      <p>
+        <span><i class="fas fa-check-circle icon"></i></span>
+        <strong>Lorem Ipsum</strong>
+      </p>
+      <p class="p-text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin augue
+        ipsum, aliquam et est quis, maximus tempus ante.
+      </p>
+    </article>
+    <article>
+      <p>
+        <span><i class="fas fa-check-circle icon"></i></span>
+        <strong>Maecenas egestas</strong>
+      </p>
+      <p class="p-text">
+        Maecenas egestas orci mollis nibh pretium, sit amet lacinia mauris
+        blandit.
+      </p>
+    </article>
+    <article>
+      <p>
+        <span><i class="fas fa-check-circle icon"></i></span>
+        <strong>Suspendisse faucibus felis</strong>
+      </p>
+      <p class="p-text">
+        Suspendisse faucibus felis id bibendum rhoncus. Phasellus neque metus,
+        varius id ultrices in, bibendum eget dolor. Ut augue felis, sodales ud
+        diam nec, congue vehicula turpis.
+      </p>
+    </article>
+    <footer class="footer-position">
+      <p style="padding-left: 21px; margin-bottom: 23px">Privacy & terms</p>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+p {
+  margin: 0;
+}
+
+article {
+  margin-bottom: 16px;
+}
+
+.icon {
+  width: 16px;
+  height: 16px;
+  color: #635bff;
+  margin-right: 12px;
+}
+
+.p-text {
+  margin-left: 28px;
+}
+</style>
