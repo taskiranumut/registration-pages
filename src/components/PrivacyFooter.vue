b3a7b8e51b69fec6929b18ae184e2c374475d364
umuttaskiran <umut.taskiran7@gmail.com>
2021-11-09 14:04:36 +0300

    add PrivacyFooter component

---
+++ src/components/PrivacyFooter.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "PrivacyFooter",
+};
+</script>
+
+<template>
+  <footer class="footer-position">
+    <p>Privacy & terms</p>
+  </footer>
+</template>
+
+<style scoped>
+footer p {
+  padding-left: 21px;
+  margin-bottom: 23px;
+  cursor: pointer;
+}
+</style>
